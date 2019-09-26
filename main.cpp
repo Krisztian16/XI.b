f2fb8b51bd4bb5ecd0e4a593bfd5a6ff8950719a
Krisztian16 <55728934+Krisztian16@users.noreply.github.com>
2019-09-26 10:45:14 +0300

    6/1
    6/1

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
-#include <stdlib.h>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    int n=5,i,j;
-    int matrix[n][n];
+    int n,m,i,j,a,k,b;
+    ifstream in ("input.txt");
+    ofstream out ("output.txt");
+    in>>n;
+    in>>m;
+    int ma[n][m];
     for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            matrix[i][j]=rand()%10;
-            if (i==j)matrix[i][j]=0;
-            if (i>j)matrix[i][j]=-matrix[i][j];
-            cout<<matrix[i][j]<<" ";
-        }cout<<endl;
+        for(j=0;j<m;j++){
+                k=0;
+            in>>a;
+            ma[i][j]=a;
+               for(b=2;b<=a/2 && k==0;b++){
+                 if(a%b==0)k=1;
+                 }
+                 if(k==0)out<<"1"<<" ";else out<<"0"<<" ";
+        }out<<endl;
     }
 
-
-
-    return 0;
+        return 0;
 }
